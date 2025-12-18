ca623f0e490a84a7614cd9238915f10eff92a37f
Artem <a.ermizin.corp@yandex.ru>
2025-12-18 23:12:25 +0700

    Создать общие классы и реализовать метод addNewTrainingSession в классе Timetable (1)

--- src/DayOfWeek.java
+++ src/DayOfWeek.java
@@ -1,4 +1,3 @@
-package PACKAGE_NAME;
-
 public enum DayOfWeek {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
 }
--- src/Timetable.java
+++ src/Timetable.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.TreeMap;
 
 public class Timetable {
+
+    private Map<DayOfWeek, TreeMap<TimeOfDay, Group>> timetable = new HashMap<>();
+
+    public void addNewTrainingSession(TrainingSession trainingSession) {
+        //сохраняем занятие в расписании
+        TreeMap<TimeOfDay, Group> timetableByTime = timetable.get(trainingSession.getDayOfWeek());
+
+        if (timetableByTime == null) {
+            timetableByTime = new TreeMap<>();
+            timetable.put(trainingSession.getDayOfWeek(), timetableByTime);
+        }
+
+        timetableByTime.put(trainingSession.getTimeOfDay(), trainingSession.getGroup());
+    }
+
+    public /* непонятно, что возвращать */ getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    }
+
+    public /* непонятно, что возвращать */ getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    }
+
 }
